348e908299fe552e38c2a432df3ab0e05bcc50a0
Keyur Pipaliya123 <112421106+Keyurpipaliya@users.noreply.github.com>
2025-12-11 12:45:24 +0530

    Add files via upload

---
+++ DSA_Array Programms/array8.cpp
@@ -0,0 +1,57 @@
+//  C++ Program to rotating elements by a certain number of positions in given array
+
+// Array rotation mean shifting element left or right by a fixed number of positions.
+
+// (Left Rotation by k = 2)
+
+// original array : 1 2 3 4 5 6
+// after rotation : 3 4 5 6 1 2
+
+#include <iostream>
+
+using namespace std;
+
+// function to reverse array from start to end
+
+void reverse(int arr[], int start, int end)
+{
+
+  while (start < end)
+  {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    start++;
+    end--;
+  }
+}
+     
+int main(){
+
+  int arr[] = {10, 20, 30, 40, 50};
+  int n = 5;
+  int k = 4;
+
+  k = k % n; // k < n
+
+  // step 1
+
+  reverse(arr, 0, k - 1); // 0 - 2 // [10] [20] [30] =[30] [20] [10]
+
+  // step 2
+
+  reverse(arr , k , n - 1); // 3 - 4 // [40] [50] = [50] [40]
+
+  // step
+
+  reverse(arr , 0 , n - 1); // 0 - 4 //[30] [20] [10] [40] [50] = [40] [50] [10] [20] [30]
+
+  cout << "Array after rotation: "; 
+
+  for(int i = 0; i < n; i++){
+    cout << arr[i] << " ";
+  }
+
+  return 0;
+}
